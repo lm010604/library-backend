84438fe582d5461d278e011d27ff1ec8a8cbebc3
Lauren Mok <lm685@cornell.edu>
2025-08-12 23:47:55 +0800

    Fixed bug

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,9 +8,6 @@ class BooksController < ApplicationController
           .order(created_at: :desc)
           .limit(30)
     end
-    @books.each do |book|
-      EnrichBookJob.perform_later(book.id) unless book.enriched?
-    end
     if logged_in?
       @personalized_sections = current_user.categories.includes(:books).map do |category|
         {
@@ -23,8 +20,10 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    enrichment = GoogleBooksLookup.enrich(@book)
-    @book.update(enrichment.compact) if enrichment.present?
+    if @book.description.blank?
+      description = GoogleBooksLookup.description_for(@book)
+      @book.update(description: description) if description.present?
+    end
     @description = @book.description
     per_page = 6
     @page = params[:page].to_i
