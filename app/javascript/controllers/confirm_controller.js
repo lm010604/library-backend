321a17adf14cde2513ad5e7cfd0507984aec254d
lm010604 <139633015+lm010604@users.noreply.github.com>
2025-08-13 15:40:12 +0800

    Replace confirm dialogs with modals

---
+++ app/javascript/controllers/confirm_controller.js
@@ -0,0 +1,15 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["modal"]
+
+  open(event) {
+    event.preventDefault()
+    this.modalTarget.classList.add("open")
+  }
+
+  close(event) {
+    event.preventDefault()
+    this.modalTarget.classList.remove("open")
+  }
+}
