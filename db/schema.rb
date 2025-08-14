53a78f2b3063294635a1cb8ac0341e349a35f823
Lauren Mok <lm685@cornell.edu>
2025-08-14 15:20:17 +0800

    Added body to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_14_055627) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_14_071702) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -43,6 +43,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_14_055627) do
     t.datetime "updated_at", null: false
     t.integer "likes", default: 0
     t.bigint "parent_id"
+    t.text "body"
     t.index ["parent_id"], name: "index_comments_on_parent_id"
     t.index ["review_id"], name: "index_comments_on_review_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
