845e8b7b4cb4f858de6747990f7533ba4997666c
lm010604 <139633015+lm010604@users.noreply.github.com>
2025-10-24 12:31:57 -0400

    Fix Stimulus controller loading

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,17 @@
-// Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+
+import CategorySelectorController from "./category_selector_controller"
+import CommentController from "./comment_controller"
+import ConfirmController from "./confirm_controller"
+import FavoriteScrollController from "./favorite_scroll_controller"
+import MenuController from "./menu_controller"
+import RatingController from "./rating_controller"
+import ReviewModalController from "./review_modal_controller"
+
+application.register("category-selector", CategorySelectorController)
+application.register("comment", CommentController)
+application.register("confirm", ConfirmController)
+application.register("favorite-scroll", FavoriteScrollController)
+application.register("menu", MenuController)
+application.register("rating", RatingController)
+application.register("review-modal", ReviewModalController)
